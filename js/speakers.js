a01d3440101b8e9c905c77d5697c80ad40c31635
jefBinomed <jean.francois.garreau@gmail.com>
2015-07-07 13:59:51 +0200

    chore(speakers) premier commit sur la mise en place des speakers

---
+++ js/speakers.js
@@ -0,0 +1,82 @@
+'use strict';
+/** 
+ * Sessions controller 
+ */
+devfestApp.controller('SpeakersCtrl', ['$scope', function ($scope, $http, $location) {
+	$scope.speakers = [
+		{
+			firstname : "Cyril",
+			lastname : "Mottier",
+			image : "cmottier_360x360.jpeg",
+			ribon : {
+				class : "gde",
+				title : "GDE",
+				link : "https://developers.google.com/experts/",
+				tile_long : "Google Developer Expert"
+			},
+			company : "Captain Train", 
+			about : "Passionné de technologies et de design, Cyril est un amoureux d'android et un développeur aux multiples talents. Il est aussi très actif dans la communauté android où il anime régulièrement des sessions et publie des articles sur son blog.",
+			socials : [{				
+				class : "google-plus",
+				link : "https://plus.google.com/+CyrilMottier/"
+				},{				
+				class : "twitter",
+				link : "https://twitter.com/cyrilmottier"
+				},{				
+				class : "github",
+				link : "https://github.com/cyrilmottier"
+				}
+			]
+		},
+		{
+			firstname : "CommitStrip",
+			lastname : "",
+			image : "commitstrip_360x360.jpeg",
+			ribon : {
+				class : "gdg",
+				title : "Googler",
+				tile_long : "Googler"
+			},
+			company : "CommitStrip", 
+			about : "bla bla bla",
+			socials : []
+		},
+		{
+			firstname : "François",
+			lastname : "Beaufort",
+			image : "fbeaufort_360x360.jpg",
+			ribon : {
+				class : "gdgw",
+				title : "Googler",
+				tile_long : "Googler"
+			},
+			company : "Google", 
+			about : "bla bla bla",
+			socials : []
+		},
+		{
+			firstname : "Geoffrey",
+			lastname : "Dorne",
+			image : "gdorne_360x360.jpeg",
+			company : "Human & Design", 
+			about : "bla bla bla",
+			socials : []
+		},
+		{
+			firstname : "Raphaël",
+			lastname : "Goetter",
+			image : "rgoetter_360x360.jpeg",
+			company : "Alsacréations", 
+			about : "bla bla bla",
+			socials : []
+		},
+		{
+			firstname : "Didier",
+			lastname : "Girard",
+			image : "dgirard_360x360.jpg",
+			company : "Sfeir", 
+			about : "bla bla bla",
+			socials : []
+		}
+	];
+}]);
