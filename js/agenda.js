a4c93dc3f8394b3a1bfc33969fd6f17f7735904d
jefBinomed <jean.francois.garreau@gmail.com>
2015-09-18 01:44:02 +0200

    fix(#118) c'est corrigé mais c'est tout moche le code :p @ju si tu veux faire mieux vas y avec grand plaisir !  ps: il est 1h43 du mat donc j'ai envie d'aller me coucher

--- js/agenda.js
+++ js/agenda.js
@@ -797,5 +797,26 @@ devfestApp.controller('AgendaCtrl', ['$scope', 'AgendaService', function ($scope
 
     $scope.agenda = agendaService;
 
+    var first = true;
+    $scope.repeatFinish = function(){
+    	if (first){
+    		first = false;
+    	}else{
+    		window.angularReadyForSticky = true;
+    		if (window.domReadyForSticky){
+	    		setTimeout(function() {  			
+		    		var navHeight = jQuery('#top-header').height();
+					var headerHeight = jQuery('.track-header').first().height();
+					jQuery('.stick-header').stick_in_parent({
+					    sticky_class: 'sticky',
+					    offset_top: navHeight
+					});
+					jQuery('.stick-label').stick_in_parent({
+					    offset_top: navHeight + headerHeight
+					});
+				}, 500);
+    		}
+    	}
+    }
 
 }]);
--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
 var devfestApp = angular.module('devfest2015', ['ngSanitize']);
+
+devfestApp.directive('ngcDone', function ($timeout) {  
+    return function (scope, element, attrs) {  
+        scope.$watch(attrs.ngcDone, function (callback) {  
+  
+            if (scope.$last === undefined) {  
+                scope.$watch('htmlElement', function () {  
+                    if (scope.htmlElement !== undefined) {  
+                        $timeout(eval(callback), 1);  
+                    }  
+                });  
+            }  
+  
+            if (scope.$last) {  
+                eval(callback)();  
+            }  
+        });  
+    }  
+});  
