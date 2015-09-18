9e7c225bc0ecddfe5114eb480f883d09b460d125
Julien Landuré <julien.landure@gmail.com>
2015-09-18 17:15:31 +0200

    move to v18 for the weekend ;)

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -2,4 +2,4 @@ echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to FORMATION_DEPLOY_NAME"
-google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v17 --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v18 --quiet dist/app.yaml
