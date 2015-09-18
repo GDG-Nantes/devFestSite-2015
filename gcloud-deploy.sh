f0416e9a1b754bdaded3f5d3de883c8d9c6df974
Julien Landuré <julien.landure@gmail.com>
2015-09-18 12:08:12 +0200

    fix(deploy): appengine deployment in sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -2,4 +2,4 @@ echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to FORMATION_DEPLOY_NAME"
-google-cloud-sdk/bin/gcloud preview app deploy --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --quiet dist/app.yaml
