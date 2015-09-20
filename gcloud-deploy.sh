fbc9dad50003a5e9e68093e3810440da40fc5f36
Julien Landuré <julien.landure@gmail.com>
2015-09-20 20:23:06 +0200

    Update gcloud-deploy.sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,5 +1,5 @@
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
-echo "Deploying to FORMATION_DEPLOY_NAME"
-google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v18 --quiet dist/app.yaml
+echo "Deploying to devfest"
+google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v19 --quiet dist/app.yaml
