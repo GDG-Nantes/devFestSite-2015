220e7407eb696fe1619d7478d632f7f13f1f6200
Julien Landuré <julien.landure@gmail.com>
2015-11-02 23:29:54 +0100

    move to v27

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -3,4 +3,4 @@ echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to devfest"
-google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v26 --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v27 --quiet dist/app.yaml
