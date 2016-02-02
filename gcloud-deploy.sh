6405482642b2377c5da1216b4fa0fef57ef2e486
Julien Landuré <julien.landure@gmail.com>
2016-02-02 23:40:10 +0100

    chore(deploy)

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,7 +1,7 @@
-google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
+#google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
 google-cloud-sdk/bin/gcloud config set app/promote_by_default false
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to devfest"
-google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v32 --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project devfestnantes2015 preview app deploy --version v33 --quiet dist/app.yaml
