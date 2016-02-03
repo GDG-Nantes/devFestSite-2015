c7480dd6f26db356e00ad886e92b333a866e3b7e
Julien Landuré <julien.landure@gmail.com>
2016-02-03 09:22:00 +0100

    chore(deploy)

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,4 +1,4 @@
-#google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
+google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
 google-cloud-sdk/bin/gcloud config set app/promote_by_default false
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
