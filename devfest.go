4f67920adb70603031dfb147bcdb503e93dc470c
Julien Landuré <julien.landure@gmail.com>
2016-02-03 09:49:20 +0100

    chore(deploy)

--- devfest.go
+++ devfest.go
@@ -31,7 +31,7 @@ var challenges = map[string]string{
 func challengeHandler(w http.ResponseWriter, r *http.Request) {
 	challenge := strings.Split(r.RequestURI, "/.well-known/acme-challenge/")[1]
 	if responseToChallenge, ok := challenges[challenge]; !ok {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		http.Error(w, "Error", http.StatusNotFound)
 		return
 	} else {
 		fmt.Fprintf(w, "%s", responseToChallenge)
